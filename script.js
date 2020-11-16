3b5c135de03ace156dac8e721395de0ae53110d8
GustMangust <60368251+GustMangust@users.noreply.github.com>
2020-11-17 02:51:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,29 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "flex";
 }
+
+
+
+
+var last;
+var counter=0;
+$(function() {
+    
+    $(".list__link").on('click', function() {
+        
+        if(counter==0)
+        {  
+            last = $(this);
+            $(".list__link").css('text-decoration', 'none');
+            $(this).css('border-bottom', 'solid 3px #f1cdb3');
+            counter++;
+        }
+        else
+        {
+            last.css('border-bottom','0px');
+            $(".list__link").css('text-decoration', 'none');
+            $(this).css('border-bottom', 'solid 3px #f1cdb3');
+            last = $(this);
+        }
+    });
+});
